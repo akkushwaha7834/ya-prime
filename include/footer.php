a6c3f8dacd93641bb1d6a2afd1534221ff325c95
sumit <sk03950@gmail.com>
2024-01-25 14:40:15 +0530

    add dependencies

---
+++ include/footer.php
@@ -0,0 +1,7 @@
+<script src="./vendor/jquery/jquery.min.js"></script>
+<script src="./vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="./vendor/font-awesome/js/all.min.js"></script>
+<script src="./js/app.js"></script>
+</body>
+
+</html>
