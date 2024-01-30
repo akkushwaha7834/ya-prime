d94d32fcfd778403a114a94034c3ab70d8097c52
akkushwaha7834 <akkushwaha018@gmail.com>
2024-01-30 15:46:49 +0530

    process section done

---
+++ process.php
@@ -0,0 +1,37 @@
+<?php include_once('./include/header.php') ?>
+
+<section class="process_section">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-sm-6 sm-padding">
+                <div class="work-process-content text-center">
+                    <img src="images/icon-4.png" alt="icon">
+                    <h3>Create Account</h3>
+                </div>
+                <div class="arrows"></div>
+            </div>
+            <div class="col-lg-3 col-sm-6 sm-padding">
+                <div class="work-process-content text-center">
+                    <img src="images/icon-5.png" alt="icon">
+                    <h3>Verify Account</h3>
+                </div>
+                <div class="arrows"></div>
+            </div>
+            <div class="col-lg-3 col-sm-6 sm-padding">
+                <div class="work-process-content text-center">
+                    <img src="images/icon-6.png" alt="icon">
+                    <h3>Make Identification</h3>
+                </div>
+                <div class="arrows"></div>
+            </div>
+            <div class="col-lg-3 col-sm-6 sm-padding">
+                <div class="work-process-content text-center">
+                    <img src="images/icon-2.png" alt="icon">
+                    <h3>Exchange crypto currency</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include_once('./include/footer.php') ?>
