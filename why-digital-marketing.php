377fa7e521116971384608d855c14171e158b518
akkushwaha7834 <akkushwaha018@gmail.com>
2024-01-30 16:54:02 +0530

    why digital marketing section done

---
+++ why-digital-marketing.php
@@ -0,0 +1,45 @@
+<?php include_once('./include/header.php') ?>
+
+<section class="why_dm">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h4 class="dm_cards_h4 text-center">
+            Why?
+          </h4>
+          <h3 class="dm_cards_sub_heading text-center">Why digital marketing</h3>
+      </div>
+    </div>
+    <div class="row dm_row_two">
+      <div class="col-md-4">
+        <div class="dm_image_box">
+          <img src="images/bitcoin.png" class="dm_box_image">
+          <div class="dm_box_content">
+            <h4>Expand Your Reach</h4>
+            <p>Digital marketing allows you to connect with a global audience, breaking geographical barriers and reaching potential customers wherever they are.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="dm_image_box_one">
+          <img src="images/bitcoin.png" class="dm_box_image">
+          <div class="dm_box_content">
+            <h4>Data-Driven Insights</h4>
+            <p>Gain valuable insights into consumer behaviour through data analytics, enabling you to refine your strategies and target your audience more effectively.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="dm_image_box">
+          <img src="images/bitcoin.png" class="dm_box_image">
+          <div class="dm_box_content">
+            <h4>Cost-Effective Marketing</h4>
+            <p>Digital marketing often provides a more cost-effective alternative to traditional advertising, ensuring you get the most out of your marketing budget.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php include_once('./include/footer.php') ?>
