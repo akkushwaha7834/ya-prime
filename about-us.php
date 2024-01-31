73ce01e7ed52d873d14dbe27048f6477278e83a1
akkushwaha7834 <akkushwaha018@gmail.com>
2024-01-31 12:59:24 +0530

    about us pages started

---
+++ about-us.php
@@ -0,0 +1,18 @@
+<?php include_once('./include/header.php') ?>
+
+<section class="banner">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1>About Us</h1>
+				<div class="breadcrumb">
+					<a href="index.php">Home</a>
+					<span class="fa fa-angle-double-right"></span>
+					<span class="current">About us</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php include_once('./include/footer.php') ?>
