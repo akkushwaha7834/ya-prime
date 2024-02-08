75511c642244183b0798ce425ebc764de64d9ffd
akkushwaha7834 <akkushwaha018@gmail.com>
2024-02-08 15:06:59 +0530

    8 process step created and added in home page

--- process.php
+++ process.php
@@ -34,4 +34,171 @@
     </div>
 </section>
 
+
+<section class="step_process">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h2 class="step_heading">
+          Setup your own forex brokerage in 8 Steps
+        </h2>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-3">
+        <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+          <button class="nav-link active" id="v-pills-step-one-tab" data-bs-toggle="pill" data-bs-target="#v-pills-step-one" type="button" role="tab" aria-controls="v-pills-step-one" aria-selected="true">Step One</button>
+          <button class="nav-link" id="v-pills-step-two-tab" data-bs-toggle="pill" data-bs-target="#v-pills-step-two" type="button" role="tab" aria-controls="v-pills-step-two" aria-selected="false">Step Two</button>
+          <button class="nav-link" id="v-pills-step-three-tab" data-bs-toggle="pill" data-bs-target="#v-pills-step-three" type="button" role="tab" aria-controls="v-pills-step-three" aria-selected="false">Step Three</button>
+          <button class="nav-link" id="v-pills-step-four-tab" data-bs-toggle="pill" data-bs-target="#v-pills-step-four" type="button" role="tab" aria-controls="v-pills-step-four" aria-selected="false">Step Four</button>
+          <button class="nav-link" id="v-pills-step-five-tab" data-bs-toggle="pill" data-bs-target="#v-pills-step-five" type="button" role="tab" aria-controls="v-pills-step-five" aria-selected="false">Step Five</button>
+          <button class="nav-link" id="v-pills-step-six-tab" data-bs-toggle="pill" data-bs-target="#v-pills-step-six" type="button" role="tab" aria-controls="v-pills-step-six" aria-selected="false">Step Six</button>
+          <button class="nav-link" id="v-pills-step-seven-tab" data-bs-toggle="pill" data-bs-target="#v-pills-step-seven" type="button" role="tab" aria-controls="v-pills-step-seven" aria-selected="false">Step Seven</button>
+          <button class="nav-link" id="v-pills-step-eight-tab" data-bs-toggle="pill" data-bs-target="#v-pills-step-eight" type="button" role="tab" aria-controls="v-pills-step-eight" aria-selected="false">Step Eight</button>
+        </div>
+      </div>
+      <div class="col-md-9">
+        <div class="tab-content tab_content" id="v-pills-tabContent">
+          <div class="tab-pane fade show active" id="v-pills-step-one" role="tabpanel" aria-labelledby="v-pills-step-one-tab" tabindex="0">
+            <div class="container">
+              <div class="row align-items-center">
+                <div class="col-md-6">
+                  <h3>
+                    COMPANY FORMATION
+                  </h3>
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                  </p>
+                </div>
+                  <div class="col-md-6">
+                    <img src="images/why.png" class="img-fluid">
+                  </div>
+                </div>
+              </div>
+            </div>
+          <div class="tab-pane fade" id="v-pills-step-two" role="tabpanel" aria-labelledby="v-pills-step-two-tab" tabindex="0">
+            <div class="container">
+              <div class="row align-items-center">
+                <div class="col-md-6">
+                  <h3>
+                    BANK ACCOUNT OF COMPANY
+                  </h3>
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                  </p>
+                </div>
+                <div class="col-md-6">
+                  <img src="images/why.png" class="img-fluid">
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="tab-pane fade" id="v-pills-step-three" role="tabpanel" aria-labelledby="v-pills-step-three-tab" tabindex="0">
+            <div class="container">
+              <div class="row align-items-center">
+                <div class="col-md-6">
+                  <h3>
+                    PHYSICAL ADDRESS
+                  </h3>
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                  </p>
+                </div>
+                <div class="col-md-6">
+                  <img src="images/why.png" class="img-fluid">
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="tab-pane fade" id="v-pills-step-four" role="tabpanel" aria-labelledby="v-pills-step-four-tab" tabindex="0">
+            <div class="container">
+              <div class="row align-items-center">
+                <div class="col-md-6">
+                  <h3>
+                    WEBSITE & LOGO
+                  </h3>
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                  </p>
+                </div>
+                <div class="col-md-6">
+                  <img src="images/why.png" class="img-fluid">
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="tab-pane fade" id="v-pills-step-five" role="tabpanel" aria-labelledby="v-pills-step-five-tab" tabindex="0">
+            <div class="container">
+              <div class="row align-items-center">
+                <div class="col-md-6">
+                  <h3>
+                    CRM
+                  </h3>
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                  </p>
+                </div>
+                <div class="col-md-6">
+                  <img src="images/why.png" class="img-fluid">
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="tab-pane fade" id="v-pills-step-six" role="tabpanel" aria-labelledby="v-pills-step-six-tab" tabindex="0">
+            <div class="container">
+              <div class="row align-items-center">
+                <div class="col-md-6">
+                  <h3>
+                    TRADING PLATFORM
+                  </h3>
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                  </p>
+                </div>
+                <div class="col-md-6">
+                  <img src="images/why.png" class="img-fluid">
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="tab-pane fade" id="v-pills-step-seven" role="tabpanel" aria-labelledby="v-pills-step-seven-tab" tabindex="0">
+            <div class="container">
+              <div class="row align-items-center">
+                <div class="col-md-6">
+                  <h3>
+                    SERVER SETUP
+                  </h3>
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                  </p>
+                </div>
+                <div class="col-md-6">
+                  <img src="images/why.png" class="img-fluid">
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="tab-pane fade" id="v-pills-step-eight" role="tabpanel" aria-labelledby="v-pills-step-eight-tab" tabindex="0">
+            <div class="container">
+              <div class="row align-items-center">
+                <div class="col-md-6">
+                  <h3>
+                    LIQUIDITY
+                  </h3>
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                  </p>
+                </div>
+                <div class="col-md-6">
+                  <img src="images/why.png" class="img-fluid">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
 <?php include_once('./include/footer.php') ?>
