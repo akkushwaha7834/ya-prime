d2ee3b4f8d85857ae11b386f2b125ca2f4877e9d
akkushwaha7834 <akkushwaha018@gmail.com>
2024-02-13 16:48:44 +0530

    career page

---
+++ career-details.php
@@ -0,0 +1,86 @@
+<?php include_once('./include/header.php') ?>
+
+<section class="careers_banner">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h1>Careers Details</h1>
+        <div class="breadcrumb">
+          <a href="./">Home</a>
+          <span class="fa fa-angle-double-right"></span>
+          <span class="current">Careers Details</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="career_details_section">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 ">
+				<div class="careers_details_section">
+					<h2 class="careers_heading">
+						Job Description
+					</h2>
+					<h3>
+						Senior Analyst
+					</h3>
+					<h4>
+						Job Summary
+					</h4>
+					<ul>
+						<li>
+							Location: Chennai
+						</li>
+						<li>
+							Project role: Senior analyst
+						</li>
+						<li>
+							Qualification: B-Tech
+						</li>
+						<li>
+							Experience: 4.5-8 Years
+						</li>
+						<li>
+							Skills: FEA Analysis
+						</li>
+						<li>
+							No. of positions: 25
+						</li>
+					</ul>
+					<h4>
+						Job Description:
+					</h4>
+					<p>
+						To understand the design specifications| conduct analysis| deliver the results of analysis done on it as per the defined quality and time standards and suggest design modification in case of structural failure.
+					</p>
+					<ul>
+						<li>
+							To gather information and to prepare the methodology for the analysis
+						</li>
+						<li>
+							To perform analysis and resolve the issues encountered
+						</li>
+						<li>
+							To perform the quality checks throughout the process
+						</li>
+						<li>
+							To prepare the model for the analysis and build technical model
+						</li>
+						<li>
+							To validate the results and prepare the reports
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="careers_sticky_sidebar">
+					<button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">Request Quote</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php include_once('./include/footer.php') ?>
---
+++ careers.php
@@ -0,0 +1,103 @@
+<?php include_once('./include/header.php') ?>
+
+<section class="careers_banner">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h1>Careers</h1>
+        <div class="breadcrumb">
+          <a href="./">Home</a>
+          <span class="fa fa-angle-double-right"></span>
+          <span class="current">Careers</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="careers_details">
+	<div class="container">
+		<div class="row align-items-center">			
+			<div class="col-md-6 text-center">
+				<img src="images/careers_image.webp" class="img-fluid" width="60%">
+			</div>
+			<div class="col-md-6">
+				<h2>Join Us Today</h2>
+				<p>At YaPrime, we're all about learning, growing, and innovating together as a team. As a company, we're committed to progress – for our customers and employees. We welcome those with creativity, drive, agility, and a passion for thinking differently.</p>
+			</div>
+		</div>
+		<div class="row mt-5">
+			<div class="col-md-12">
+				<h2 class="mb-4 text-center">Our Values</h2>
+			</div>
+			<div class="col-md-4">
+				<div class="careers_image_box">
+					<div class="careers_image_content mb-3">
+						<img src="images/proudness.png" class="img-fluid">
+						<h4>Take pride</h4>
+					</div>
+					<div class="careers_content_image">
+						<p>At YaPrime, we're all about learning, growing, and innovating together as a team. As a company, we're committed to progress – for our customers and employees. We welcome those with creativity, drive, agility, and a passion for thinking differently.</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="careers_image_box">
+					<div class="careers_image_content mb-3">
+						<img src="images/proudness.png" class="img-fluid">
+						<h4>Take pride</h4>
+					</div>
+					<div class="careers_content_image">
+						<p>At YaPrime, we're all about learning, growing, and innovating together as a team. As a company, we're committed to progress – for our customers and employees. We welcome those with creativity, drive, agility, and a passion for thinking differently.</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="careers_image_box">
+					<div class="careers_image_content mb-3">
+						<img src="images/proudness.png" class="img-fluid">
+						<h4>Take pride</h4>
+					</div>
+					<div class="careers_content_image">
+						<p>At YaPrime, we're all about learning, growing, and innovating together as a team. As a company, we're committed to progress – for our customers and employees. We welcome those with creativity, drive, agility, and a passion for thinking differently.</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="careers_image_box">
+					<div class="careers_image_content mb-3">
+						<img src="images/proudness.png" class="img-fluid">
+						<h4>Take pride</h4>
+					</div>
+					<div class="careers_content_image">
+						<p>At YaPrime, we're all about learning, growing, and innovating together as a team. As a company, we're committed to progress – for our customers and employees. We welcome those with creativity, drive, agility, and a passion for thinking differently.</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="careers_image_box">
+					<div class="careers_image_content mb-3">
+						<img src="images/proudness.png" class="img-fluid">
+						<h4>Take pride</h4>
+					</div>
+					<div class="careers_content_image">
+						<p>At YaPrime, we're all about learning, growing, and innovating together as a team. As a company, we're committed to progress – for our customers and employees. We welcome those with creativity, drive, agility, and a passion for thinking differently.</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="careers_image_box">
+					<div class="careers_image_content mb-3">
+						<img src="images/proudness.png" class="img-fluid">
+						<h4>Take pride</h4>
+					</div>
+					<div class="careers_content_image">
+						<p>At YaPrime, we're all about learning, growing, and innovating together as a team. As a company, we're committed to progress – for our customers and employees. We welcome those with creativity, drive, agility, and a passion for thinking differently.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php include_once('./include/footer.php') ?>
