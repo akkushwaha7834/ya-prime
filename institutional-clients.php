f332ed3f0e5f7650538a8a63091f09b5852b8784
akkushwaha7834 <akkushwaha018@gmail.com>
2024-02-15 12:37:53 +0530

    Created Institutional Clients page

--- institutional-clients.php
+++ institutional-clients.php
@@ -1,4 +1,20 @@
 <?php include_once('./include/header.php') ?>
 
+<section class="institutional_clients_banner">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1>Institutional Clients</h1>
+				<div class="breadcrumb">
+					<a href="index.php">Home</a>
+					<span class="fa fa-angle-double-right"></span>
+					<span class="current">Institutional Clients</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php include_once('./include/pricing.php') ?>
 
 <?php include_once('./include/footer.php') ?>
