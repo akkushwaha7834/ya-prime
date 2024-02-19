12bda76bed4a8ecb64f6edfb5ac557f7fa4710bd
akkushwaha7834 <akkushwaha018@gmail.com>
2024-02-19 15:53:00 +0530

    portfolio page done

--- portfolio.php
+++ portfolio.php
@@ -1,4 +1,99 @@
 <?php include_once('./include/header.php') ?>
 
+<section class="blogs_banner">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h1>Blogs</h1>
+        <div class="breadcrumb">
+          <a href="./">Home</a>
+          <span class="fa fa-angle-double-right"></span>
+          <span class="current">Blogs</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="portfolio_start">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+		        <h4 class="dm_cards_h4 text-center">
+		            Our Portfolio
+		          </h4>
+		          <h3 class="dm_cards_sub_heading text-center">Why YaPrime</h3>
+		      </div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="row mt-5">
+			<div class="col-md-8 position-relative">
+				<div class="content-overlay"></div>
+				<img src="images/port.jpeg" class="img-fluid">
+			</div>
+			<div class="col-md-4">
+				<div class="port_content">
+					<h2 class="port_number">
+						01
+					</h2>
+					<h4 class="port_name">
+						Industry Expertise
+					</h4>
+					<p class="port_details">
+						YaPrime has years of experience within the forex industry. This experience has provided valuable insights into market dynamics, allowing us to solve complexities effectively. We stay informed about the latest trends, regulations, and shifts in the forex market, ensuring our solutions remain relevant and compliant. Our proven track record of success reflects our ability to deliver results and drive growth for our clients.
+					</p>
+					<div class="port_link">
+						<a href="#" class="port_link">Visit Website</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row mt-5">
+			<div class="col-md-4">
+				<div class="port_content">
+					<h2 class="port_number">
+						02
+					</h2>
+					<h4 class="port_name">
+						Advanced Technology
+					</h4>
+					<p class="port_details">
+						We're committed to employing modern technology to provide our clients with powerful and efficient solutions. This commitment ensures that our services are at the forefront of industry standards. Innovation is at the core of what we do. We continuously explore new technologies and approaches to maintain our competitive edge. Our solutions seamlessly integrate with leading platforms, enhancing their functionality and efficiency.
+					</p>
+					<div class="port_link">
+						<a href="#" class="port_link">Visit Website</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-8 position-relative">
+				<div class="content-overlay"></div>
+				<img src="images/port.jpeg" class="img-fluid">
+			</div>
+		</div>
+		<div class="row mt-5">
+			<div class="col-md-8 position-relative">
+				<div class="content-overlay"></div>
+				<img src="images/port.jpeg" class="img-fluid">
+			</div>
+			<div class="col-md-4">
+				<div class="port_content">
+					<h2 class="port_number">
+						03
+					</h2>
+					<h4 class="port_name">
+						Trust and Reliability
+					</h4>
+					<p class="port_details">
+						Trust is paramount in the forex industry. We establish trust by maintaining transparent and ethical practices in all our dealings. Clients can rely on YaPrime as a dependable partner for their forex brokerages. We consistently deliver high-quality services and support. Our commitment to trust and reliability ensures that our clients can operate with confidence and peace of mind.
+					</p>
+					<div class="port_link">
+						<a href="#" class="port_link">Visit Website</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
 
 <?php include_once('./include/footer.php') ?>
