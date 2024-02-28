43f1a533451478667aaca75e6d2fe75cd073300e
akkushwaha7834 <akkushwaha018@gmail.com>
2024-02-28 16:46:52 +0530

    removed mouse effect

--- js/app.js
+++ js/app.js
@@ -1,34 +1,3 @@
-if (screen.width > 991) {
-  // custom mouse
-  var cursor = document.querySelector(".cursor");
-  var cursorinner = document.querySelector(".cursor2");
-  var a = document.querySelectorAll("a");
-  document.addEventListener("mousemove", (e) => {
-    cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`;
-  });
-  document.addEventListener("mousemove", (e) => {
-    var x = e.clientX;
-    var y = e.clientY;
-    cursorinner.style.left = x + "px";
-    cursorinner.style.top = y + "px";
-  });
-  document.addEventListener("mousedown", () => {
-    cursor.classList.add("click");
-    cursorinner.classList.add("cursorinnerhover");
-  });
-  document.addEventListener("mouseup", () => {
-    cursor.classList.remove("click");
-    cursorinner.classList.remove("cursorinnerhover");
-  });
-  a.forEach((item) => {
-    item.addEventListener("mouseover", () => {
-      cursor.classList.add("hover");
-    });
-    item.addEventListener("mouseleave", () => {
-      cursor.classList.remove("hover");
-    });
-  });
-}
 
 $(document).ready(function () {
   // $(".dropdown").hover(
